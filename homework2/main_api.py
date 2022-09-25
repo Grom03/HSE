deb8af8cf61a1a1b11ff95a3b45ac7fcb1da353e
Grom03 <aagromov_3@edu.hse.ru>
2022-09-25 21:19:47 +0300

    if incorrect url return 405 code

--- homework2/main_api.py
+++ homework2/main_api.py
@@ -1,4 +1,5 @@
 import flask
+from werkzeug.exceptions import HTTPException
 
 app = flask.Flask(__name__)
 values = dict()
@@ -14,9 +15,9 @@ def hello():
 @app.route('/set', methods=['POST'])
 def app_set():
     if not 'Content-Type' in flask.request.headers or flask.request.headers['Content-Type'] != 'application/json':
-        flask.abort(415)
+        return flask.Response(status=415)
     if not flask.request or not 'key' in flask.request.json or not 'value' in flask.request.json:
-        flask.abort(400)
+        return flask.Response(status=400)
     values[flask.request.json['key']] = flask.request.json['value']
     return flask.Response()
 
@@ -24,7 +25,7 @@ def app_set():
 @app.route('/get/<string:key>', methods=['GET'])
 def app_get(key):
     if key not in values:
-        flask.abort(404)
+        return flask.Response(status=404)
     ans = {
         "key": key,
         "value": values[key]
@@ -39,34 +40,19 @@ def app_get(key):
 @app.route('/devide', methods=['POST'])
 def devide():
     if not 'Content-Type' in flask.request.headers or flask.request.headers['Content-Type'] != 'application/json':
-        flask.abort(415)
+        return flask.Response(status=415)
     if not flask.request or not 'dividend' in flask.request.json or not 'divider' in flask.request.json:
-        flask.abort(400)
+        return flask.Response(status=400)
     if flask.request.json['divider'] == 0:
-        flask.abort(400)
+        return flask.Response(status=400)
     response = flask.Response(str(flask.request.json['dividend'] / flask.request.json['divider']))
     response.headers['Content-Type'] = 'text/plain'
     return response
 
 
-@app.errorhandler(400)
-def not_found(error):
-    return flask.make_response('', 400)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return flask.make_response('', 404)
-
-
-@app.errorhandler(405)
-def not_found(error):
-    return flask.make_response('', 405)
-
-
-@app.errorhandler(415)
-def not_found(error):
-    return flask.make_response('', 415)
+@app.errorhandler(HTTPException)
+def handle_exception(error):
+    return flask.Response(status=405)
 
 
 if __name__ == '__main__':
